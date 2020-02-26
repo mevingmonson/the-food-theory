c68e22f4d4a010539f5501e08ef6eac07b8496cc
Mevin G Monson <mevingmonson@gmail.com>
2020-02-26 18:25:41 +0530

    search module commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,48 @@
 // Global app controller
-console.log("hello worldscsdcsddsvsdvsdvsd213!");
+import Search from './models/Search';
+import { elements } from './views/base';
+import * as searchView from './views/searchView';
+
+
+/*** Global State of the app
+ * Search object
+ * current recipe object
+ * Shopping list obj
+ * Liked recipe
+ */
+
+const state = {};
+
+//Search Controller
+const controlSearch = async () => {
+    //1/ Get query from view
+    const query = searchView.getInput();
+
+
+    if (query) {
+        //2 New search obj and add to state
+        state.search = new Search(query);
+
+        //3 prepare ui for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        //4 Search for recipes
+        await state.search.getResults();
+
+        //5 Render results on UI
+        console.log(state.search.result);
+        searchView.renderResults(state.search.result)
+
+    }
+}
+
+
+
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -13,6 +13,17 @@ module.exports = {
     plugins: [new htmlWebpackPlugin({
         filename: 'index.html',
         template: './src/index.html'
-    })]
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 
 };
